3b58677adea0d64252cea67b9953a1f8d2845301
Scott Merrill <skippy@skippy.net>
2011-12-06 20:45:35 -0500

    initial Habari 0.7 SQLite commit

---
+++ php/config.php
@@ -0,0 +1,26 @@
+<?php
+// Habari OpenShift config file
+// any files in the /php directory of an OpenShift application will be
+// deleted with every update. This means that the SQLite database files
+// needs to live outside of the webroot.
+
+// https://www.redhat.com/openshift/kb/kb-e1002/deploying-openshift-express-applications-using-sqlite
+
+$path = str_replace('runtime/repo/php', 'data', dirname(__FILE__));
+
+Config::set( 'db_connection', array(
+	'connection_string'=>"sqlite:$path/habari.db",
+	'username'=>'',
+	'password'=>'',
+	'prefix'=>''
+));
+
+$blog_data = array( 'blog_title' => 'Habari',
+	'admin_username' => 'admin',
+	'admin_pass1' => 'habari',
+	'admin_pass2' => 'habari',
+	'admin_email' => 'CHANGE@ME',
+);
+
+//$locale = '{$locale}';
+?>
